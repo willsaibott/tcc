f065b88302bed7cd203f15ba935fc780fcbc1f0b
willsaibott <guinbcab@hotmail.com>
2017-07-16 19:53:54 -0300

    PD - 002 - Development Completed

---
+++ TCC/Assets/Script/Errors.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public class Errors {
+    public const int NO_ERRORS                      = 0x0000;
+    public const int ON_IMPORT_PATH_EMPTY           = 0x0001;
+    public const int ON_IMPORT_FILE_NOT_FOUND       = 0x0002;
+    public const int ON_IMPORT_FILE_NOT_SUPPORTED   = 0x0003;
+
+    public static Dictionary<int, string> _MAP = new Dictionary<int, string>()
+    {
+        { NO_ERRORS,                    ErrorsMessages._MAP["NO_ERRORS"] },
+        { ON_IMPORT_FILE_NOT_FOUND,     ErrorsMessages._MAP["ON_IMPORT_FILE_NOT_FOUND"] },
+        { ON_IMPORT_PATH_EMPTY,         ErrorsMessages._MAP["ON_IMPORT_PATH_EMPTY"] },
+        { ON_IMPORT_FILE_NOT_SUPPORTED, ErrorsMessages._MAP["ON_IMPORT_FILE_NOT_SUPPORTED"] }
+    };
+}
---
+++ TCC/Assets/Script/ImportManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImportManager : MonoBehaviour {
+    private GameObject importedObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnImportObject() {
+        Debug.Log("Importing Object");
+
+        string path = GetPath();
+        int    code = ImportObject(path);
+
+        if (code == Errors.NO_ERRORS) {
+            Debug.Log(Errors._MAP[Errors.NO_ERRORS]);
+        } else {
+            Debug.LogError(Errors._MAP[code]);
+        }
+    }
+
+    int ImportObject(string path) {
+        int code = Errors.NO_ERRORS;
+        if (path == "") {
+            code = Errors.ON_IMPORT_PATH_EMPTY;
+        }
+        return code;
+    }
+
+    string GetPath() {
+        return "";
+    }
+
+
+}
